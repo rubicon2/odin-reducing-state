95b7309c729adf907af0ae10fac93850cdbca6a0
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-07-17 12:45:55 +0100

    Style Chat component

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,4 +1,22 @@
 import { useAppState, useAppDispatch } from '../../contexts/AppContext';
+import Container from '../container';
+import Heading from '../heading';
+import styled from 'styled-components';
+
+const ChatContainer = styled(Container)`
+  background-color: lightblue;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+
+  textarea {
+    resize: none;
+  }
+`;
 
 export default function Chat(props) {
   const { className } = props;
@@ -6,21 +24,28 @@ export default function Chat(props) {
   const dispatch = useAppDispatch();
 
   return (
-    <div data-testid="chat" className={className}>
-      <h2>Chat</h2>
-      <textarea
-        name="message"
-        id="message"
-        aria-label="chat"
-        placeholder="Type your message..."
-        value={state.messages[state.selectedId] || ''}
-        onChange={(event) =>
-          dispatch({ type: 'message_edited', message: event.target.value })
-        }
-      />
-      <button type="button" onClick={() => dispatch({ type: 'message_sent' })}>
-        Send
-      </button>
-    </div>
+    <ChatContainer data-testid="chat" className={className}>
+      <Heading level={2}>Chat</Heading>
+      <InputContainer>
+        <textarea
+          name="message"
+          id="message"
+          aria-label="chat"
+          placeholder="Type your message..."
+          rows={10}
+          cols={75}
+          value={state.messages[state.selectedId] || ''}
+          onChange={(event) =>
+            dispatch({ type: 'message_edited', message: event.target.value })
+          }
+        />
+        <button
+          type="button"
+          onClick={() => dispatch({ type: 'message_sent' })}
+        >
+          Send
+        </button>
+      </InputContainer>
+    </ChatContainer>
   );
 }
